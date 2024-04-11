bb700737ffeb16c0adc1029399036aa3e338f72d
Devagya Tak <thegreattak94@gmail.com>
2024-04-11 11:26:50 +0530

    completed ui

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './sidebar.css'
 import { assets } from '../../assets/assets'
 const Sidebar = () => {
+    const [extended, setExtended] = useState(false)
+
     return (
         <div className='sidebar'>
             <div className="top">
-                <img className='menu' src={assets.menu_icon} alt="" />
+                <img className='menu' onClick={() => { setExtended(!extended) }} src={assets.menu_icon} alt="" />
                 <div className="new-chat">
                     <img src={assets.plus_icon} alt="" className="" />
-                    <p>New Chat</p>
+                    {extended &&<p>New Chat</p>}
                 </div>
+                {extended
+                &&
                 <div className="recent">
                     <p className="recent-title">Recent</p>
                     <div className="recent-entry">
@@ -17,19 +21,20 @@ const Sidebar = () => {
                         <p>What is react</p>
                     </div>
                 </div>
+                }
             </div>
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} alt="" />
-                    <p>Help</p>
+                    {extended && <p>Help</p>}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} alt="" />
-                    <p>Activity</p>
+                    {extended && <p>Activity</p>}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} alt="" />
-                    <p>Settings</p>
+                    {extended && <p>Settings</p>}
                 </div>
             </div>
         </div>
